da293ab6f90aa21b40e4f535d727d70486c9ddf9
visenze <visenze@visenzes-MacBook-Pro.local>
2019-06-24 14:58:24 +0800

    Setting up colour scheme

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 # Install oh my zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install vim runtime
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Set up colour molokai
+git clone https://github.com/tomasr/molokai.git
+mv molokai/colors ~/.vim
+rm -rf molokai
 
 # Copy .vimrc and .tmux.conf to user directory
 cp .vimrc ~/.vimrc
